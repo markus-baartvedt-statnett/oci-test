230c0d7c82b2cb24b8216c401a81f1b4438b94f2
Markus Baartvedt <markus.baartvedt@statnett.no>
2025-08-13 14:06:05 +0200

    Changed logic for current workspace

--- locals.tf
+++ locals.tf
@@ -8,7 +8,13 @@ locals {
     }
 }
 
-# Current workspace
+
 locals {
-    workspace = { for k, v in oci_identity_compartment.compartments : k => v.id if k == terraform.workspace }
+    # All compartments
+    all_compartments = merge(
+        { for k, v in local.active_compartments : k => v if k == terraform.workspace },
+        { for k, v in oci_identity_compartment.compartments : k => v.id if k == terraform.workspace }
+    )
+    # Current workspace
+    workspace = local.all_compartments[terraform.workspace]
 }
