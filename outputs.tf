243da8da72004cf8439c9bc636112041ebfb8639
Markus Baartvedt <markus.baartvedt@statnett.no>
2025-08-13 15:53:33 +0200

    Removed ip info from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,27 +5,6 @@ output "compartment_ids" {
   }
 }
 
-output "webserver_ids" {
-  value = { for env, value in oci_core_instance.webservers : env => value.id }
-}
-
-output "webserver_private_ips" {
-  value = {
-    for env, instance in oci_core_instance.webservers : env => instance.create_vnic_details[0].private_ip
-  }
-}
-
-output "PublicLoadBalancer_Public_IPs" {
-  value = {
-    for env, loadbalancer in oci_load_balancer.PublicLoadBalancer :
-    env => loadbalancer.ip_addresses
-  }
-}
-
 output "workspace" {
   value = terraform.workspace
-}
-
-output "dhcp_options_keys" {
-  value = keys(oci_core_dhcp_options.dhcp_options)
 }
