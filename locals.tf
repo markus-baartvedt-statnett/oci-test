c593bd59b598f4f3b54eef4371a9c473b6941891
Markus Baartvedt <markus.baartvedt@statnett.no>
2025-08-14 08:51:30 +0200

    Fixed missing variable local.workspace not declared, swapped with local.all_compartments in locals.tf

--- locals.tf
+++ locals.tf
@@ -1,9 +1,10 @@
 # Local variables
 locals {
-    compartment_keys = sort(keys(local.workspace))
+    compartment_keys = sort(keys(local.all_compartments))
 
     # Assign a unique index to each compartment key
     compartment_cidr_indices = {
         for i, key in local.compartment_keys : key => i
     }
+    
 }
