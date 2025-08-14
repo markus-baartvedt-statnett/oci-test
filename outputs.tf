70a554fdc1e3628f12633632f780aa8b513f975f
Markus Baartvedt <markus.baartvedt@statnett.no>
2025-08-14 09:50:11 +0200

    Added output from loadbalancers and updated the bootstrap file

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,13 @@
 # Outputs
 output "compartment_ids" {
   value = {
-    for env, compartment in local.workspace : env => compartment
+    for env, compartment in local.all_compartments : env => compartment
+  }
+}
+
+output "loadbalancer" {
+  value = {
+    for env, loadbalancer in oci_load_balancer.PublicLoadBalancer : env => loadbalancer
   }
 }
 
