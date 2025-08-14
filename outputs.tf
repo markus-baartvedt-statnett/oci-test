d27944e98dcb559ae58774c22dbf1eb6995d4e75
Markus Baartvedt <markus.baartvedt@statnett.no>
2025-08-14 10:23:23 +0200

    Added instance_ip to output

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,18 @@
 # Outputs
 output "compartment_ids" {
-  value = {
-    for env, compartment in local.all_compartments : env => compartment
+    value = {
+        for env, compartment in local.all_compartments : env => compartment
   }
 }
 
 output "loadbalancer_ip" {
-  value = oci_load_balancer.PublicLoadBalancer.ip_address_details.ip_address
+    value = oci_load_balancer.PublicLoadBalancer.ip_address_details.ip_address
+}
+
+output "instance_ip" {
+    value = oci_core_instance.webserver.private_ip
 }
 
 output "workspace" {
-  value = terraform.workspace
+    value = terraform.workspace
 }
